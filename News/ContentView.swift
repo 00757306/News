74f1d2115cc417765025f0643c00b5513ac1524f
Carol <s110416045@stu.ntue.edu.tw>
2020-03-22 13:44:55 +0800

    Initial Commit

---
+++ News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News
+//
+//  Created by Ong Wei Yee on 2020/3/22.
+//  Copyright © 2020 Carol. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
